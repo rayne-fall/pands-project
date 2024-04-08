7100bb59ec745d96e449a7489071e4f406db13ee
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2024-04-08 21:10:02 +0100

    imported modules and added code for saving plots as PNGs

--- analysis.py
+++ analysis.py
@@ -1,3 +1,9 @@
 # analysis
 # Author: Sylvia Chapman Kent
 # analyses Fisher's Iris data set
+
+# importing the modules we'll use during the analysis
+import matplotlib as plt
+import numpy as np
+
+plt.savefig('variable.png') # this function will save a plot as a PNG file (title is a placeholder)

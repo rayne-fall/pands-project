1dff8824208aaab3f776da6a8bd87f97d987a69b
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2024-04-09 11:42:30 +0100

    began creating functions for generating plots

--- analysis.py
+++ analysis.py
@@ -5,5 +5,17 @@
 # importing the modules we'll use during the analysis
 import matplotlib as plt
 import numpy as np
+import pandas as pd
 
-plt.savefig('variable.png') # this function will save a plot as a PNG file (title is a placeholder)
+# creating a function to generate histograms as we'll need one for each variable
+def generate_hist(hist_title):
+    histogram_data = ()
+    plt.hist(histogram_data) # creates the histogram
+    plt.savefig(hist_title) # this function will save a plot as a PNG file 
+
+# creating another function to generate scatter plots
+def generate_scatter(x,y):
+    x = np.array[()]
+    y = np.array[()]
+    plt.scatter(x,y) # creates the scatter plot
+    plt.show # shows the scatter plot

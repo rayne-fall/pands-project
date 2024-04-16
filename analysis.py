66d033dcda4614c432010bd19db29d22f9da73bc
Sylvia Chapman Kent <sylvia.kent.2.1@hotmail.com>
2024-04-16 12:36:29 +0100

    program no longer layers each histogram over the previous ones

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@
 # Author: Sylvia Chapman Kent
 # analyses Fisher's Iris data set
 
-# Program currently layers each histogram over the previous ones: this is unintended 
+# Program no longer layers each histogram over the previous ones
 
 # importing the modules we'll use during the analysis
 import matplotlib.pyplot as plt
@@ -14,10 +14,14 @@ df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/
 
 # creating a function to generate histograms as we'll need one for each variable
 def generate_hist(variable):
-    histogram_data = variable # gets the data for the histogram
-    histogram_data = histogram_data.to_numpy() # converts data frame to array so we can plot it
-    plt.hist(histogram_data) # creates the histogram
-    plt.savefig(plot_name) # saves the plot as a PNG file 
+    fig, ax = plt.subplots()
+    histogram_data = variable # get the data for the histogram
+    histogram_data = histogram_data.to_numpy() # convert data frame to array so we can plot it
+    ax.hist(histogram_data) # create the histogram
+    ax.set_xlabel("Number of flowers") # label the x axis
+    ax.set_ylabel(f"{plot_name} (cm)") # label the y axis based on the variable we're looking at
+    ax.set_title(plot_name) # put a title on the plot
+    fig.savefig(plot_name) # save the plot as a PNG file 
 '''   
 # creating another function to generate scatter plots
 def generate_scatter(x,y):
@@ -27,18 +31,18 @@ def generate_scatter(x,y):
     plt.show() # shows the scatter plot
 '''
 
-variable = df["sepal_length"] # we want to plot the sepal lengths
-plot_name = ("Sepal Length") 
-histogram = generate_hist(variable)
+variable = df["sepal_length"] # choose the variable to plot
+plot_name = ("Sepal Length") # give a filename for the PNG
+histogram = generate_hist(variable) # call the histogram function defined earlier
 
-variable = df["sepal_width"] # we want to plot the sepal widths
+variable = df["sepal_width"] 
 plot_name = ("Sepal Width") 
 histogram = generate_hist(variable) 
 
-variable = df["petal_length"] # we want to plot the petal lengths
+variable = df["petal_length"] 
 plot_name = ("Petal Length") 
 histogram = generate_hist(variable) 
 
-variable = df["petal_width"] # we want to plot the petal widths
+variable = df["petal_width"] 
 plot_name = ("Petal Width") 
 histogram = generate_hist(variable) 
